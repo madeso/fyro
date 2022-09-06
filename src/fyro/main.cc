bcc85223a0b325c4f05cb1e7b2036ba032e759b0
Gustav <sir.gustav.the.coder@gmail.com>
2022-09-07 00:49:07 +0200

    feat: basic fyro engine working

--- src/fyro/main.cc
+++ src/fyro/main.cc
@@ -1,184 +1,421 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <optional>
+
+#include <nlohmann/json.hpp>
+#include "physfs.h"
+
 #include "fyro/fyro.h"
+#include "lox/lox.h"
+#include "lox/printhandler.h"
 
-#include <array>
+using json = nlohmann::json;
+namespace fs = std::filesystem;
 
-#include "fyro/random.h"
 
-constexpr std::array<int, 64> endesga64 =
+struct Exception
 {
-	0xff0040, 0x131313, 0x1b1b1b, 0x272727, 0x3d3d3d, 0x5d5d5d, 0x858585, 0xb4b4b4,
-	0xffffff, 0xc7cfdd, 0x92a1b9, 0x657392, 0x424c6e, 0x2a2f4e, 0x1a1932, 0x0e071b,
-	0x1c121c, 0x391f21, 0x5d2c28, 0x8a4836, 0xbf6f4a, 0xe69c69, 0xf6ca9f, 0xf9e6cf,
-	0xedab50, 0xe07438, 0xc64524, 0x8e251d, 0xff5000, 0xed7614, 0xffa214, 0xffc825,
-	0xffeb57, 0xd3fc7e, 0x99e65f, 0x5ac54f, 0x33984b, 0x1e6f50, 0x134c4c, 0x0c2e44,
-	0x00396d, 0x0069aa, 0x0098dc, 0x00cdf9, 0x0cf1ff, 0x94fdff, 0xfdd2ed, 0xf389f5,
-	0xdb3ffd, 0x7a09fa, 0x3003d9, 0x0c0293, 0x03193f, 0x3b1443, 0x622461, 0x93388f,
-	0xca52c9, 0xc85086, 0xf68187, 0xf5555d, 0xea323c, 0xc42430, 0x891e2b, 0x571c27
-};
+	std::vector<std::string> errors;
 
-struct ColorFlips
-{
-	static constexpr int width = 100;
-	static constexpr int height = 100;
-	static constexpr float update_time = 15.0f;
-	static constexpr float max_time = 15.0;
-	static constexpr float size = 1.7f;
-	static constexpr float spacing = 2.0f;
-	static constexpr float startx = 0.0f;
-	static constexpr float starty = 0.0f;
-
-	float timer[width * height] = { 0.0f, };
-	std::size_t index[width * height] = { 0, };
-
-	ColorFlips()
+	Exception append(const std::string& str)
 	{
-		auto r = Random{};
-
-		for(std::size_t i=0; i<width*height; i+=1) { timer[i] = r.get_including(max_time); }
-		for(std::size_t i=0; i<width*height; i+=1) { index[i] = r.get_excluding(endesga64.size()); }
+		errors.emplace_back(str);
+		return *this;
 	}
 
-	void update(float dt)
+	Exception append(const std::vector<std::string>& li)
 	{
-		for(std::size_t i=0; i<width*height; i+=1)
+		for(const auto& str: li)
 		{
-			timer[i] -= dt;
-			if(timer[i] < 0.0f)
-			{
-				timer[i] += update_time;
-				index[i] = (index[i] + 1) % endesga64.size();
-			}
+			errors.emplace_back(str);
+		}
+		return *this;
+	}
+};
+
+Exception collect_exception()
+{
+	try
+	{
+		throw;
+	}
+	catch(const Exception& e)
+	{
+		return e;
+	}
+	catch(const std::exception& e)
+	{
+		return {{e.what()}};
+	}
+	catch(...)
+	{
+		return {{"unknown errror"}};
+	}
+}
+
+
+std::string get_physfs_error()
+{
+	const auto code = PHYSFS_getLastErrorCode();
+	return PHYSFS_getErrorByCode(code);
+}
+
+Exception physfs_exception(const std::string& message)
+{
+	const std::string error = get_physfs_error();
+	return Exception {{message + error}};
+}
+
+static PHYSFS_EnumerateCallbackResult physfs_callback(void *data, const char *origdir, const char *fname)
+{
+	auto* ret = static_cast<std::vector<std::string>*>(data);
+	ret->emplace_back(std::string(origdir) + fname);
+
+	// std::cout << "file " << fname << " in " << origdir << "\n";
+	return PHYSFS_ENUM_OK;
+}
+
+std::vector<std::string> physfs_files_in_dir(const std::string& dir)
+{
+	std::vector<std::string> r;
+	auto ok = PHYSFS_enumerate(dir.c_str(), &physfs_callback, &r);
+
+	if(ok == 0)
+	{
+		std::cerr << "failed to enumerate " << dir << "\n";
+	}
+
+	return r;
+}
+
+std::string read_file_to_string(const std::string& path)
+{
+	auto* file = PHYSFS_openRead(path.c_str());
+	if(file == nullptr)
+	{
+		// todo(Gustav): split path and find files in dir
+		throw physfs_exception("unable to open file").append("found files").append(physfs_files_in_dir(""));
+	}
+
+	std::vector<char> ret;
+	while(PHYSFS_eof(file) == 0)
+	{
+		char buffer[1024];
+		const auto read = PHYSFS_readBytes(file, buffer, 1024);
+		if(read > 0)
+		{
+			ret.insert(ret.end(), buffer, buffer+read);
 		}
 	}
 
-	void render(render::SpriteBatch* batch)
-	{
-		const auto color = [](int c, int s) -> float
-		{
-			return static_cast<float>((c >> s) & 0xff) / 255.0f;
-		};
+	ret.emplace_back('\0');
 
-		for(std::size_t x=0; x<width; x+=1)
-		for(std::size_t y=0; y<height; y+=1)
+	PHYSFS_close(file);
+	return ret.data();
+}
+
+
+json load_json(const std::string& path)
+{
+	const auto src = read_file_to_string(path);
+	return json::parse(src);
+}
+
+
+struct GameData
+{
+	std::string title = "fyro";
+	int width = 800;
+	int height = 600;
+};
+
+GameData load_game_data(const std::string& path)
+{
+	try
+	{
+		json data = load_json(path);
+
+		auto r = GameData{};
+		r.title  = data["title"] .get<std::string>();
+		r.width  = data["width"] .get<int>();
+		r.height = data["height"].get<int>();
+		return r;
+	}
+	catch(...)
+	{
+		auto x = collect_exception();
+		x.errors.push_back("while reading " + path);
+		throw x;
+	}
+}
+
+struct PrintLoxError : lox::PrintHandler
+{
+	void on_line(std::string_view line) override
+	{
+		std::cerr << line << "\n";
+	}
+};
+
+struct Rgb { float r; float g; float b; };
+
+struct RenderData
+{
+	const render::RenderCommand& rc;
+	std::optional<render::RenderLayer2> layer;
+
+	explicit RenderData(const render::RenderCommand& rr) : rc(rr) {}
+	~RenderData()
+	{
+		if(layer)
 		{
-			const auto color_index = index[x * width + y];
-			const auto c = endesga64[color_index];
-			batch->quad
-			(
-				{},
-				Rectf{size, size}
-					.translate(startx, starty)
-					.translate(static_cast<float>(x)*spacing, static_cast<float>(y)*spacing),
-				{}, {color(c, 2), color(c, 1), color(c, 0), 1.0f});
+			layer->batch->submit();
 		}
 	}
 };
 
-#if 0
-struct ExampleGame : public State
+struct RenderArg
 {
-	render::Texture cards;
-	render::Texture letter_g;
-	ColorFlips flips;
-	glm::vec2 mouse;
+	std::shared_ptr<RenderData> data;
+};
 
-	ExampleGame()
-		: cards
-		(
-			::cards::load_texture()
-		)
-		, letter_g
-		(
-			render::load_image_from_embedded
-			(
-				LETTER_G_PNG,
-				render::TextureEdge::clamp,
-				render::TextureRenderStyle::smooth,
-				render::Transparency::include
-			)
-		)
-		, mouse(0, 0)
+struct State
+{
+	State() = default;
+	virtual ~State() = default;
+	virtual void update(float) = 0;
+	virtual void render(const render::RenderCommand& rc) = 0;
+};
+
+struct ScriptState : State
+{
+	std::shared_ptr<lox::Instance> instance;
+	lox::Lox* lox;
+
+	std::shared_ptr<lox::Callable> on_update;
+	std::shared_ptr<lox::Callable> on_render;
+
+	ScriptState(std::shared_ptr<lox::Instance> in, lox::Lox* lo)
+		: instance(in)
+		, lox(lo)
 	{
+		on_update = instance->get_bound_method_or_null("update");
+		on_render = instance->get_bound_method_or_null("render");
 	}
 
-	void
-	on_update(float dt) override
+	void update(float dt) override
 	{
-		flips.update(dt);
+		if(on_update)
+		{
+			on_update->call({{lox::make_number_float(static_cast<double>(dt))}});
+		}
 	}
 
-	void
-	on_render(const render::RenderCommand& rc) override
+	void render(const render::RenderCommand& rc) override
 	{
-		auto r = render::with_layer2(rc, render::LayoutData{render::ViewportStyle::extended, 200.0f, 200.0f});
-
-		r.batch->quad({}, r.viewport_aabb_in_worldspace, {}, {0.8, 0.8, 0.8, 1.0f});
-
-		r.batch->submit();
-
-		flips.render(r.batch);
-
-		constexpr auto card_sprite = Cint_to_float(::cards::back).zero().set_height(30);
-
-		r.batch->quad(&cards, card_sprite.translate(100, 100), ::cards::hearts[2]);
-		r.batch->quad(&cards, card_sprite.translate(r.mouse_to_world(mouse)), ::cards::back);
-
-		r.batch->quad(&letter_g, Rectf{40, 40}.translate(40, 40), {});
-	}
-
-	void on_mouse_position(const render::InputCommand&, const glm::ivec2& p) override
-	{
-		// todo(Gustav): store world mouse?
-		mouse = {p.x, p.y};
+		if(on_render)
+		{
+			auto ra = std::make_shared<RenderData>(rc);
+			auto render = lox->make_native<RenderArg>(RenderArg{ra});
+			on_render->call({{render}});
+			// todo(Gustav): clean up render object here so calling functions on it can crash nicely instead of going down in assert/crash hell
+		}
 	}
 };
-#else
+
 struct ExampleGame : public Game
 {
-	ColorFlips flips;
-	glm::vec2 mouse;
+	lox::Lox lox;
+
+	std::unique_ptr<State> next_state;
+	std::unique_ptr<State> state;
 
 	ExampleGame()
-		: mouse(0, 0)
+		: lox(std::make_unique<PrintLoxError>(), [](const std::string& str)
+		{
+			std::cout << str << "\n";
+		})
 	{
+		bind_functions();
+	}
+
+	void run_main()
+	{
+		const auto src = read_file_to_string("main.lox");
+		if(false == lox.run_string(src))
+		{
+			throw Exception{{"Unable to run script"}};
+		}
+	}
+
+	void bind_functions()
+	{
+		auto fyro = lox.in_package("fyro");
+		
+		fyro->define_native_function("set_state", [this](lox::Callable*, lox::ArgumentHelper& arguments)
+		{
+			auto instance = arguments.require_instance();
+			arguments.complete();
+			next_state = std::make_unique<ScriptState>(instance, &lox);
+			return lox::make_nil();
+		});
+
+		fyro->define_native_class<Rgb>("Rgb", [](lox::ArgumentHelper& ah) -> Rgb
+		{
+			const auto r = static_cast<float>(ah.require_float());
+			const auto g = static_cast<float>(ah.require_float());
+			const auto b = static_cast<float>(ah.require_float());
+			ah.complete();
+			return Rgb{r, g, b};
+		});
+
+		// todo(Gustav): validate argumends and raise script error on invalid
+		fyro->define_native_class<RenderArg>("RenderCommand")
+			.add_function("windowbox", [](RenderArg& r, lox::ArgumentHelper& ah)
+			{
+				const auto width = static_cast<float>(ah.require_float());
+				const auto height = static_cast<float>(ah.require_float());
+				ah.complete();
+				r.data->layer = render::with_layer2(r.data->rc, render::LayoutData{render::ViewportStyle::black_bars, width, height});
+				return lox::make_nil();
+			})
+			.add_function("clear", [](RenderArg& r, lox::ArgumentHelper& ah)
+			{
+				auto color = ah.require_native<Rgb>();
+				ah.complete();
+				r.data->layer->batch->quad({}, r.data->layer->viewport_aabb_in_worldspace, {}, {color->r, color->g, color->b, 1.0f});
+				return lox::make_nil();
+			})
+			.add_function("rect", [](RenderArg& r, lox::ArgumentHelper& ah)
+			{
+				auto color = ah.require_native<Rgb>();
+				const auto x = static_cast<float>(ah.require_float());
+				const auto y = static_cast<float>(ah.require_float());
+				const auto width = static_cast<float>(ah.require_float());
+				const auto height = static_cast<float>(ah.require_float());
+				ah.complete();
+				r.data->layer->batch->quad({},
+					Rect{width, height}.translate(x, y),
+					{}, {color->r, color->g, color->b, 1.0f}
+				);
+				return lox::make_nil();
+			})
+		;
 	}
 
 	void
 	on_update(float dt) override
 	{
-		flips.update(dt);
+		if(state)
+		{
+			state->update(dt);
+		}
+		if(next_state != nullptr)
+		{
+			state = std::move(next_state);
+		}
 	}
 
 	void
 	on_render(const render::RenderCommand& rc) override
 	{
-		auto r = render::with_layer2(rc, render::LayoutData{render::ViewportStyle::extended, 200.0f, 200.0f});
-
-		r.batch->quad({}, r.viewport_aabb_in_worldspace, {}, {0.8, 0.8, 0.8, 1.0f});
-
-		r.batch->submit();
-
-		flips.render(r.batch);
+		if(state)
+		{
+			state->render(rc);
+		}
+		else
+		{
+			// todo(Gustav): draw some basic thing here since to state is set?
+			auto r = render::with_layer2(rc, render::LayoutData{render::ViewportStyle::extended, 200.0f, 200.0f});
+			r.batch->quad({}, r.viewport_aabb_in_worldspace, {}, {0.8, 0.8, 0.8, 1.0f});
+			r.batch->submit();
+		}
 	}
 
-	void on_mouse_position(const render::InputCommand&, const glm::ivec2& p) override
+	void on_mouse_position(const render::InputCommand&, const glm::ivec2&) override
 	{
-		// todo(Gustav): store world mouse?
-		mouse = {p.x, p.y};
 	}
 };
-#endif
 
 
-int
-main(int, char**)
+struct Physfs
 {
+	Physfs(const Physfs&) = delete;
+	Physfs(Physfs&&) = delete;
+	Physfs operator=(const Physfs&) = delete;
+	Physfs operator=(Physfs&&) = delete;
+
+	Physfs(const std::string& path)
+	{
+		const auto ok = PHYSFS_init(path.c_str());
+		if(ok == 0)
+		{
+			throw physfs_exception("unable to init");
+		}
+	}
+
+	~Physfs()
+	{
+		const auto ok = PHYSFS_deinit();
+		if(ok == 0)
+		{
+			const std::string error = get_physfs_error();
+			std::cerr << "Unable to shut down physfs: " << error << "\n";
+		}
+	}
+
+	void setup(const std::string& root)
+	{
+		std::cout << "sugggested root is: " << root << "\n";
+		PHYSFS_mount(root.c_str(), "/", 1);
+	}
+};
+
+
+int run(int argc, char** argv)
+{
+	auto physfs = Physfs(argv[0]);
+	
+	if(argc == 2)
+	{
+		fs::path folder = argv[1];
+		folder = fs::canonical(folder);
+		physfs.setup(folder.c_str());
+	}
+	else
+	{
+		physfs.setup(PHYSFS_getBaseDir());
+	}
+
+	const auto data = load_game_data("main.json");
+
 	return run_game
 	(
-		"Example", glm::ivec2{800, 600}, false, []()
+		data.title, glm::ivec2{data.width, data.height}, false, []()
 		{
 			auto game = std::make_shared<ExampleGame>();
+			game->run_main();
 			return game;
 		}
 	);
+}
+
+
+int
+main(int argc, char** argv)
+{
+	try
+	{
+		return run(argc, argv);
+	}
+	catch(...)
+	{
+		auto x = collect_exception();
+		for(const auto& e: x.errors)
+		{
+			std::cerr << e << '\n';
+		}
+		return -1;
+	}
 }
